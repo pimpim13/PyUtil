22e959ecc1516836d1eacacc9f78f4cdafe8b0c8
pimpim13 <alain.zypinoglou@gmail.com>
2021-01-05 16:35:03 +0100

    Finalisation de la toolbar Création du fichier de reference si absent

--- src/main/python/main.py
+++ src/main/python/main.py
@@ -1,13 +1,10 @@
-from fbs_runtime.application_context.PySide2 import ApplicationContext
+from PySide2 import QtWidgets
 import sys
-# from package.main_window import MainWindow
-from package.main_window import Window, MainWindow
+from package.main_window import Window
 
 if __name__ == '__main__':
-    appctxt = ApplicationContext()       # 1. Instantiate ApplicationContext
-    # window = MainWindow()
+    appctxt = QtWidgets.QApplication(sys.argv)     # 1. Instantiate ApplicationContext
     window = Window(ctx=appctxt)
-    # window.resize(650, 550)
     window.show()
-    exit_code = appctxt.app.exec_()      # 2. Invoke appctxt.app.exec_()
+    exit_code = appctxt.exec_()      # 2. Invoke appctxt.app.exec_()
     sys.exit(exit_code)
